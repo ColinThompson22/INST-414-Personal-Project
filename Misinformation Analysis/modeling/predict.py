c397afb07c98e7088c2abbe04791c9516994c406
Colin Thompson <157647912+ColinThompson22@users.noreply.github.com>
2025-04-27 19:05:03 -0400

    Update predict.py

--- Misinformation Analysis/modeling/predict.py
+++ Misinformation Analysis/modeling/predict.py
@@ -10,20 +10,9 @@ app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+#Code for setup
+import pandas as pd
+df= pd.read_csv("birds_arent_real_tweets.csv")
 
 
 if __name__ == "__main__":
